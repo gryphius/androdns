acabf58c4b75939533e2f416a82e2f757f116461
Oli Schacher <oli@wgwh.ch>
2021-02-15 10:53:22 +0100

    rename example fragments to query/history/bookmarks

--- app/src/main/java/androdns/android/leetdreams/ch/androdns/MainActivity.kt
+++ app/src/main/java/androdns/android/leetdreams/ch/androdns/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                R.id.nav_query, R.id.nav_history, R.id.nav_bookmarks), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
