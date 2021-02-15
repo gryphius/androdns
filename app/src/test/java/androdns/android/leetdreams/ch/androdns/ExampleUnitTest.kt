21f081dc1cb51e04c0d3458c7a9d15422da94988
Oli Schacher <oli@wgwh.ch>
2021-02-15 10:38:17 +0100

    start with a completely new project on kotlin

---
+++ app/src/test/java/androdns/android/leetdreams/ch/androdns/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package androdns.android.leetdreams.ch.androdns
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
