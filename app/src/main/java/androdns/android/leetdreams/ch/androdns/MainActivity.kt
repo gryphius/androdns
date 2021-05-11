5a6ccf7e5df6e7d461edf34ceea3eef5be16fa7b
Oli Schacher <oli@wgwh.ch>
2021-05-11 20:35:08 +0200

    fragment popup test

--- app/src/main/java/androdns/android/leetdreams/ch/androdns/MainActivity.kt
+++ app/src/main/java/androdns/android/leetdreams/ch/androdns/MainActivity.kt
@@ -2,6 +2,7 @@ package androdns.android.leetdreams.ch.androdns
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -24,11 +25,9 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
+        setupGuiEvents()
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -38,6 +37,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_query, R.id.nav_history, R.id.nav_bookmarks), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun setupGuiEvents(){
+        // query send button
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Query sent(not really..)", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/androdns/android/leetdreams/ch/androdns/ui/TransportSetupFragment.kt
+++ app/src/main/java/androdns/android/leetdreams/ch/androdns/ui/TransportSetupFragment.kt
@@ -18,10 +18,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [QuerySetupFragment.newInstance] factory method to
+ * Use the [TransportSetupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QuerySetupFragment : Fragment() {
+class TransportSetupFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -43,53 +43,13 @@ class QuerySetupFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater!!.inflate(R.layout.fragment_query_setup, container, false)
 
-        // type/class dialog
-        val dialogTypeClassButton: Button = view.findViewById(R.id.buttonTypeClass)
-        dialogTypeClassButton.setOnClickListener{
-                view ->
-            Snackbar.make(view, "opening the thing", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
 
-        // transport dialog
-        val dialogTransportButton: Button = view.findViewById(R.id.buttonTransport)
-        dialogTransportButton.setOnClickListener{
-                view ->
-            Snackbar.make(view, "opening the transport thing", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            showCustomDialog();
-        }
 
         return view
     }
 
-    private lateinit var alertDialog: AlertDialog
-    fun showCustomDialog() {
-        val inflater: LayoutInflater = this.getLayoutInflater()
-        val dialogView: View = inflater.inflate(R.layout.fragment_transport_setup, null)
 
-        /*
-        val header_txt = dialogView.findViewById<TextView>(R.id.header)
-        header_txt.text = "Header Message"
-        val details_txt = dialogView.findViewById<TextView>(R.id.details)
-        val custom_button: Button = dialogView.findViewById(R.id.customBtn)
-        custom_button.setOnClickListener {
-            //perform custom action
-        }
-        */
 
-        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
-        dialogBuilder.setOnDismissListener(object : DialogInterface.OnDismissListener {
-            override fun onDismiss(arg0: DialogInterface) {
-
-            }
-        })
-        dialogBuilder.setView(dialogView)
-
-        alertDialog = dialogBuilder.create();
-        //alertDialog.window!!.getAttributes().windowAnimations = R.style.p
-        alertDialog.show()
-    }
     companion object {
         /**
          * Use this factory method to create a new instance of
